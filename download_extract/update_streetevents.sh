fc273b9311cc3f78c85352cd96befe48901e93ae
Ian Gow <iandgow@gmail.com>
2016-07-01 16:27:25 -0400

    Fixed long-time issue in StreetEvents update.
    - R was getting environment variables from `~/.Rprofile`, making it difficult to point R at different databases when updating.

--- download_extract/update_streetevents.sh
+++ download_extract/update_streetevents.sh
@@ -3,7 +3,8 @@ rsync -avz researchgrid.hbs.edu:/export/projects/streetevents_project/ \
     $EDGAR_DIR/streetevents_project/ --include=*.xml --exclude=*.zip \
     --exclude=*.7z --exclude=*.sas7bdat \
     --exclude=*.xls* --exclude=jsheridan/* --delete
+
 echo "Updating $PGDATABASE on $PGHOST from $EDGAR_DIR"
-R CMD BATCH --no-environ download_extract/create_call_files.R
-R CMD BATCH --no-environ download_extract/import_call_meta_data.R
-R CMD BATCH --no-environ download_extract/import_speaker_data.R
+Rscript --vanilla download_extract/create_call_files.R
+Rscript --vanilla download_extract/import_call_meta_data.R
+Rscript --vanilla download_extract/import_speaker_data.R
