7bd4da6e6ef07ff5c03786e07fc3c946053ed282
rudyardrichter <rudyardrichter@uchicago.edu>
2016-08-05 14:51:21 -0500

    Working on match_analysis.R

--- db_matches/match_analysis.R
+++ db_matches/match_analysis.R
@@ -1,6 +1,6 @@
 library(dplyr)
 
-pg <- src_postgres()
+pg <- src_postgres(host="aaz.chicagobooth.edu", dbname="postgres")
 
 tbl_pg <- function(table) {
     tbl(pg, sql(paste0("SELECT * FROM ", table)))
@@ -13,20 +13,61 @@ company_link <- tbl_pg("streetevents.company_link")
 executive_link <- tbl_pg("streetevents.executive_link")
 manual_permno_matches <- tbl_pg("streetevents.manual_permno_matches")
 calls <- tbl_pg("streetevents.calls")
+stocknames <- tbl_pg("crsp.stocknames")
 
 # How many firms have the same PERMNO as identified with
 # the ticker match?
 match_compare <-
     crsp_link %>%
-    inner_join(company_link, by="file_name") %>%
-    mutate(same_permno=permno.x==permno.y) %>%
+    inner_join(company_link, by="file_name", suffix=c(".crsp", ".comp")) %>%
+    mutate(same_permno=permno.crsp==permno.comp) %>%
     compute()
 
-match_compare %>%
+match_compare_summary <-
+    match_compare %>%
     group_by(same_permno) %>%
     summarize(count=n())
 
-match_compare %>%
+match_compare_diff_permno <-
+    match_compare %>%
     filter(!same_permno) %>%
-    inner_join(calls) %>%
-    View()
+    inner_join(calls)
+
+permno.crsp_permco <-
+    match_compare %>%
+    inner_join(select(stocknames, permco, permno), by=c("permno.crsp"="permno"))
+
+permno.comp_permco <-
+    match_compare %>%
+    # select(file_name, permno.y) %>%
+    inner_join(select(stocknames, permco, permno), by=c("permno.comp"="permno"))
+
+match_compare_permco <-
+    permno.crsp_permco %>%
+    inner_join(select(permno.comp_permco, permco, file_name), by="file_name",
+               suffix=c(".crsp", ".comp"))
+
+match_compare_permco_diff_permno <-
+    match_compare_permco %>%
+    filter(!same_permno) %>%
+    group_by(file_name) %>%
+    filter(row_number() == 1) %>%
+    ungroup() %>%
+    mutate(same_permco=permco.crsp==permco.comp) %>%
+    compute()
+
+diff_permco_summary <-
+    match_compare_permco_diff_permno %>%
+    group_by(same_permco) %>%
+    summarize(count=n())
+
+diff_permco <-
+    match_compare_permco_diff_permno %>%
+    filter(!same_permco)
+
+match_compare_stocks <-
+    match_compare_permco %>%
+    filter(!same_permno) %>%
+    inner_join(stocknames, by=c("permno.crsp"="permno")) %>%
+    inner_join(stocknames, by=c("permno.comp"="permno"),
+               suffix=c(".crsp1", ".comp1"))
