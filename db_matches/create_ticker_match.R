d096383e3c351c8e87561d660a69240c4ffd742e
Ian Gow <iandgow@gmail.com>
2016-08-28 18:10:23 -0400

    Fixes to ticker-match code.

--- db_matches/create_ticker_match.R
+++ db_matches/create_ticker_match.R
@@ -28,21 +28,27 @@ tickers <-
 secm_tickers <-
     secm %>%
     mutate(eomonth=eomonth(datadate)) %>%
-    select(gvkey, eomonth, datadate, tic) %>%
+    select(gvkey, eomonth, tic) %>%
     rename(ticker=tic) %>%
-    compute()
+    compute(indexes="gvkey")
 
 rdqs <-
     fundq %>%
     select(gvkey, datadate, rdq) %>%
+    filter(!is.na(rdq)) %>%
     mutate(eomonth=eomonth(rdq)) %>%
-    distinct()
+    distinct() %>%
+    compute(indexes="gvkey")
 
 # Merge tickers from comp.secm with announcement dates
 # from comp.fundq. Add PERMNOs.
 comp_tickers <-
     rdqs %>%
     inner_join(secm_tickers) %>%
+    compute(indexes=c("gvkey"))
+
+ticker_match_init <-
+    comp_tickers %>%
     inner_join(crsp_linktable) %>%
     filter(rdq >= linkdt,
            rdq <= linkenddt | is.na(linkenddt)) %>%
@@ -51,14 +57,15 @@ comp_tickers <-
 
 # Match earnings announcements with calls within three days
 # and with the same ticker
+rs <- dbGetQuery(pg$con, "DROP TABLE IF EXISTS streetevents.ticker_match")
+
 ticker_match <-
-    tickers %>%
-    inner_join(comp_tickers) %>%
+    ticker_match_init %>%
+    inner_join(tickers) %>%
     filter(between(call_date, rdq, sql("rdq + interval '3 days'"))) %>%
     select(file_name, last_update, gvkey, datadate, permno) %>%
     compute(name="ticker_match", indexes="file_name", temporary=FALSE)
 
-rs <-
-    RPostgreSQL::dbGetQuery(pg$con, "DROP TABLE IF EXISTS streetevents.ticker_match")
-rs <- RPostgreSQL::dbGetQuery(pg$con, "ALTER TABLE ticker_match SET SCHEMA streetevents")
+
+rs <- dbGetQuery(pg$con, "ALTER TABLE ticker_match SET SCHEMA streetevents")
 
