36ab1280d72a432e0ffe6a9262e70c8d4ecbab33
yolshanskiy <yolshanskiys@yandex.ru>
2017-05-10 15:08:14 -0500

    Process files with tone code #12

---
+++ linguistic_features/process_file_tone.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*
+
+import os
+from sqlalchemy import create_engine
+from tone_functions import categories, get_tone_data
+from numerical_intensity import num_count
+from fl_sents import prop_fl_sents
+
+conn_string = 'postgresql://' + os.environ['PGHOST'] + '/' + os.environ['PGDATABASE']
+engine = create_engine(conn_string)
+
+#file_name = "5189334_T"
+#file_name = files[1]
+
+input_schema = "streetevents"
+input_table = "speaker_data"
+output_schema = "streetevents"
+output_table = "tone_data"
+
+
+
+def processFile(file_name):
+
+    # Get syllable data for the file_name
+    speaker_data = getToneData(file_name)
+    for cat in categories:
+        speaker_data[cat] = speaker_data['speaker_text'].map(lambda x: get_tone_data(x, cat))    
+    speaker_data['num_count'] = speaker_data['speaker_text'].map(num_count)
+
+    speaker_data = speaker_data.drop(['speaker_text'], 1)
+
+    # Submit dataframe to database
+    conn = engine.connect()
+    speaker_data.to_sql(output_table, conn, schema=output_schema, if_exists='append',
+              index=False)
+    conn.close()
+
+def getToneData(file_name):
+    from pandas.io.sql import read_sql
+    
+    conn = engine.connect()
+    table_exists = engine.dialect.has_table(conn, output_table, schema=output_schema)
+    conn.close()
+
+    # It may be better to explicitly create the table elsewhere.
+    # Checking like this might be slower.
+    if table_exists:
+        sql = "DELETE FROM %s.%s WHERE file_name='%s'" % \
+            (output_schema, output_table, file_name)
+
+        engine.execute(sql)
+
+
+    sql = """
+       	SELECT file_name, last_update, speaker_name, employer, role, speaker_number, context, speaker_text, language
+    	FROM %s.%s
+	    WHERE file_name='%s'
+        """ % (input_schema, input_table, file_name)
+
+    df = read_sql(sql, engine)
+
+    return df
---
+++ linguistic_features/run_tone.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+import pandas as pd
+from sqlalchemy import create_engine
+from multiprocessing import Pool
+from process_file_tone import processFile, conn_string, \
+        input_table, input_schema, output_table, output_schema
+
+
+engine = create_engine(conn_string)
+
+
+def getFileNames(output_table, output_schema, num_files=None):
+    import pandas as pd
+
+    # Using LIMIT is much faster than getting all files and ditching
+    # unneeded ones.
+    if num_files==None:
+        limit_clause = ""
+    else:
+        limit_clause = "LIMIT %s" % (num_files)
+
+    # Get a list of unprocessed files. Query differs according to whether
+    # any files have been processed (i.e., output_table exists)
+    conn = engine.connect()
+    table_exists = engine.dialect.has_table(conn, output_table, schema=output_schema)
+    conn.close()
+
+    if table_exists:
+        sql = """
+            WITH latest_call AS (
+                SELECT file_name, last_update
+                FROM streetevents.calls
+                WHERE call_type=1)
+            SELECT DISTINCT file_name, last_update
+            FROM latest_call
+            EXCEPT
+            SELECT file_name, last_update
+            FROM %s.%s
+            %s
+        """ % (output_schema, output_table, limit_clause)
+        files = pd.read_sql(sql, engine)
+    else:
+        sql = """CREATE TABLE %s.%s
+                (
+                    file_name text,
+                    last_update timestamp without time zone,
+                    speaker_name text,
+                    employer text,
+                    role text,
+                    speaker_number integer,
+                    context text,
+                    language text,
+                    positive int,
+                    negative int,
+                    uncertainty int,
+                    litigious int,
+                    modal_strong int,
+                    modal_weak int,
+                    num_count int)
+            """ % (output_schema, output_table)
+        engine.execute(sql)
+
+        sql = """
+            SELECT DISTINCT file_name, last_update
+            FROM streetevents.calls
+            WHERE call_type=1
+            %s
+        """ % (limit_clause)
+        files = pd.read_sql(sql, engine)
+
+    return files
+
+# Get a list of files to work on.
+num_threads = 8
+files = getFileNames(output_table, output_schema, 100)
+files = files['file_name']
+#files.
+
+# Set up multiprocessing environment
+pool = Pool(num_threads)
+
+# Do the work!
+pool.map(processFile, files)
+
+
