6e39408037f119b525ad3aa8f7cd68308b8d7287
Ian Gow <igow@hbs.edu>
2017-05-11 17:50:45 -0400

    Tweaks to R import code.
    - Added function to unescape XML.
    - Deleted unnecessary file_list step.
    - Fixed order of columns.
    - Added option to pass file_list to process_calls function.
    - Added code to handle calls with multiple presentation and Q&A sections.
    - Resolves iangow/streetevents_private/#17.

--- import_speaker_data.R
+++ import_speaker_data.R
@@ -6,16 +6,19 @@ library(parallel)
 
 se_path <- file.path(Sys.getenv("EDGAR_DIR"), "streetevents_project")
 
+unescape_xml <- function(str) {
+    xml_text(read_html(paste0("<x>", str, "</x>")))
+}
+
 extract_speaker_data <- function(file_path) {
     full_path <- file.path(se_path, file_path)
-    # print(full_path)
     if (!file.exists(full_path)) return(NULL)
-    file_xml <- read_xml(file.path(se_path, file_path))
-    file_list <- as_list(file_xml)
-    print(file_path)
-    lines <- xml_text(xml_child(file_xml, search = "EventStory/Body"))
 
+    print(file_path)
+    file_xml <- read_xml(file.path(se_path, file_path), options = "NOENT")
+    lines <- xml_text(xml_child(file_xml, search = "/EventStory/Body"))
     lines <- gsub("\\r\\n", "\n", lines, perl = TRUE)
+    lines <- unescape_xml(lines)
 
     sections <- str_split(lines, "==={3,}\n")[[1]]
 
@@ -27,6 +30,10 @@ extract_speaker_data <- function(file_path) {
         speaker_text <- text[seq(from = 3, length.out = num_speakers, by = 2)]
         speaker <- clean_speaker(speaker_data)
         bind_cols(extract_speaker(speaker), tibble(speaker_text))
+    }
+
+    analyze_text_wrap <- function(sections) {
+        bind_rows(lapply(sections, analyze_text), .id="section")
     }
 
     clean_speaker <- function(speaker) {
@@ -54,8 +61,10 @@ extract_speaker_data <- function(file_path) {
 
     if (length(pres) > 0) {
         pres_df <-
-            analyze_text(pres) %>%
-            mutate(context = "pres")
+            analyze_text_wrap(pres) %>%
+            mutate(context = "pres") %>%
+            select(file_path, speaker_name, employer, role,
+                   speaker_number, speaker_text, context, section)
     } else {
         return(NULL)
     }
@@ -64,8 +73,10 @@ extract_speaker_data <- function(file_path) {
 
     if (length(qa) > 0) {
         qa_df <-
-            analyze_text(qa) %>%
-            mutate(context = "qa")
+            analyze_text_wrap(qa) %>%
+            mutate(context = "qa") %>%
+            select(file_path, speaker_name, employer, role,
+                   speaker_number, speaker_text, context, section)
         return(bind_rows(pres_df, qa_df))
     } else {
         return(pres_df)
@@ -86,35 +97,43 @@ if (!dbExistsTable(pg, c("streetevents", "speaker_data_alt"))) {
            employer text,
            role text,
            speaker_number integer,
+           speaker_text text,
            context text,
-           speaker_text text);
+            section integer,
+        PRIMARY KEY (file_path, speaker_number, context, section));
 
        CREATE INDEX ON streetevents.speaker_data_alt (file_path);")
 }
 rs <- dbDisconnect(pg)
 
-process_calls <- function(num_calls = 1000) {
+process_calls <- function(num_calls = 1000, file_list = NULL) {
     pg <- src_postgres()
 
-    calls <- tbl(pg, sql("SELECT * FROM streetevents.call_files ORDER BY random()"))
+    calls <- tbl(pg, sql("SELECT * FROM streetevents.call_files"))
     speaker_data_alt <- tbl(pg, sql("SELECT * FROM streetevents.speaker_data_alt"))
 
-    file_list <- calls %>%
-        anti_join(speaker_data_alt, by = "file_path") %>%
-        collect(n=num_calls)
+    if (is.null(file_list)) {
 
-    speaker_data <- do.call(bind_rows,
-                            mclapply(file_list$file_path, extract_speaker_data,
-                                     mc.cores=6))
+        file_list <-
+            calls %>%
+            arrange(random()) %>%
+            anti_join(speaker_data_alt, by = "file_path") %>%
+            collect(n=num_calls)
+    }
 
-    dbWriteTable(pg$con, c("streetevents", "speaker_data_alt"), speaker_data,
-                 row.names=FALSE, append=TRUE)
+    speaker_data <- bind_rows(mclapply(file_list$file_path, extract_speaker_data,
+                                       mc.cores=6))
+
+    dupes <-
+        speaker_data %>%
+        group_by(file_path, speaker_number, context, section) %>%
+        filter(n()>1) %>%
+        nrow() > 0
+
+    if (!dupes) {
+        dbWriteTable(pg$con, c("streetevents", "speaker_data_alt"), speaker_data,
+                     row.names=FALSE, append=TRUE)
+    }
 }
 
 system.time(process_calls())
-
-pg <- src_postgres()
-
-calls <- tbl(pg, sql("SELECT * FROM streetevents.call_files"))
-speaker_data_alt <- tbl(pg, sql("SELECT * FROM streetevents.speaker_data_alt"))
-speaker_data_alt %>% select(file_path) %>% distinct() %>% count()
