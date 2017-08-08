b98d4a66deac847568e6bf12947ffb488a32ea54
Ian Gow <iandgow@gmail.com>
2017-08-09 03:43:22 +1000

    Additional code for "new" StreetEvents.
    - Added code for `calls` table.
    - Addressed time zone issues (all "timestamp with time zone" now).
    - Use file_name and last_update to identify calls.

--- import_speaker_data.R
+++ import_speaker_data.R
@@ -10,11 +10,36 @@ unescape_xml <- function(str) {
     xml_text(read_html(paste0("<x>", str, "</x>")))
 }
 
-extract_speaker_data <- function(file_path) {
+extract_call_data <- function(file_path) {
     full_path <- file.path(se_path, file_path)
     if (!file.exists(full_path)) return(NULL)
 
+    file_name <- str_replace(file_path, "\\.xml$", "")
     file_xml <- read_xml(file.path(se_path, file_path), options = "NOENT")
+    last_update <- as.POSIXct(xml_attr(file_xml, "lastUpdate"),
+                              format="%A, %B %d, %Y at %H:%M:%S%p GMT", tz = "GMT")
+    city <- xml_text(xml_child(file_xml, search = "/city"))
+    company_name <- xml_text(xml_child(file_xml, search = "/companyName"))
+    company_ticker <- xml_text(xml_child(file_xml, search = "/companyTicker"))
+    start_date <- as.POSIXct(xml_text(xml_child(file_xml, search = "/startDate")),
+                             format="%d-%b-%y %H:%M%p GMT", tz = "GMT")
+    company_id <- xml_text(xml_child(file_xml, search = "/companyId"))
+    cusip <- xml_text(xml_child(file_xml, search = "/CUSIP"))
+    sedol <- xml_text(xml_child(file_xml, search = "/SEDOL"))
+    isin <- xml_text(xml_child(file_xml, search = "/ISIN"))
+    tibble(file_path, file_name, last_update, company_name,
+           company_ticker, start_date, company_id, cusip, sedol, isin)
+}
+
+extract_speaker_data <- function(file_path) {
+
+    full_path <- file.path(se_path, file_path)
+    if (!file.exists(full_path)) return(NULL)
+    file_name <- str_replace(file_path, "\\.xml$", "")
+
+    file_xml <- read_xml(file.path(se_path, file_path), options = "NOENT")
+    last_update <- as.POSIXct(xml_attr(file_xml, "lastUpdate"),
+                              format="%A, %B %d, %Y at %H:%M:%S%p GMT", tz = "GMT")
     lines <- xml_text(xml_child(file_xml, search = "/EventStory/Body"))
     lines <- gsub("\\r\\n", "\n", lines, perl = TRUE)
     lines <- unescape_xml(lines)
@@ -69,7 +94,8 @@ extract_speaker_data <- function(file_path) {
             employer <- NA
             role <- NA
         }
-        tibble(file_path, speaker_name, employer, role, speaker_number)
+
+        tibble(file_name, last_update, speaker_name, employer, role, speaker_number)
     }
 
     pres <- sections[grepl("^(Presentation|Transcript)\n", sections)]
@@ -78,7 +104,7 @@ extract_speaker_data <- function(file_path) {
         pres_df <-
             analyze_text_wrap(pres) %>%
             mutate(context = "pres") %>%
-            select(file_path, speaker_name, employer, role,
+            select(file_name, last_update, speaker_name, employer, role,
                    speaker_number, speaker_text, context, section)
     } else {
         return(NULL)
@@ -90,7 +116,7 @@ extract_speaker_data <- function(file_path) {
         qa_df <-
             analyze_text_wrap(qa) %>%
             mutate(context = "qa") %>%
-            select(file_path, speaker_name, employer, role,
+            select(file_name, last_update, speaker_name, employer, role,
                    speaker_number, speaker_text, context, section)
         return(bind_rows(pres_df, qa_df))
     } else {
@@ -103,48 +129,79 @@ extract_speaker_data <- function(file_path) {
 library(RPostgreSQL)
 pg <- dbConnect(PostgreSQL())
 
-if (!dbExistsTable(pg, c("streetevents", "speaker_data_alt"))) {
+if (!dbExistsTable(pg, c("streetevents", "speaker_data_new"))) {
     dbGetQuery(pg, "
-        CREATE TABLE streetevents.speaker_data_alt
+        CREATE TABLE streetevents.speaker_data_new
            (
-           file_path text,
+           file_name text,
+           last_update timestamp with time zone,
            speaker_name text,
            employer text,
            role text,
            speaker_number integer,
            speaker_text text,
            context text,
-            section integer,
-        PRIMARY KEY (file_path, speaker_number, context, section));
+           section integer,
+        PRIMARY KEY (file_name, last_update, speaker_number, context, section));
 
-       CREATE INDEX ON streetevents.speaker_data_alt (file_path);")
+       CREATE INDEX ON streetevents.speaker_data_new (file_name, last_update);")
 }
 
-if (!dbExistsTable(pg, c("streetevents", "speaker_data_dupes"))) {
+if (!dbExistsTable(pg, c("streetevents", "speaker_data_dupes_new"))) {
     dbGetQuery(pg, "
-        CREATE TABLE streetevents.speaker_data_dupes (file_path text);")
+        CREATE TABLE streetevents.speaker_data_dupes_new (file_name text, last_update timestamp with time zone);")
 }
+
 rs <- dbDisconnect(pg)
 
-process_calls <- function(num_calls = 1000, file_list = NULL) {
-    pg <- src_postgres()
+if (FALSE) {
+    library(RPostgreSQL)
+    pg <- dbConnect(PostgreSQL())
+    call_files <- tbl(pg, sql("SELECT * FROM streetevents.call_files"))
+    file_list <-
+        call_files %>%
+        select(file_path) %>%
+        pull()
+    library(parallel)
+    calls <- bind_rows(mclapply(file_list, extract_call_data, mc.cores = 12))
 
-    calls <- tbl(pg, sql("SELECT * FROM streetevents.call_files"))
-    speaker_data_alt <- tbl(pg, sql("SELECT * FROM streetevents.speaker_data_alt"))
-    speaker_data_dupes <- tbl(pg, sql("SELECT * FROM streetevents.speaker_data_dupes"))
+    rs <- dbGetQuery(pg, "SET TIME ZONE 'GMT'")
+
+    rs <- dbWriteTable(pg, c("streetevents", "calls_new"), calls,
+                       overwrite = TRUE, row.names = FALSE)
+
+    rs <- dbGetQuery(pg, "ALTER TABLE streetevents.calls_new OWNER TO streetevents")
+
+    rs <- dbGetQuery(pg, "GRANT SELECT ON TABLE streetevents.calls_new TO streetevents_access")
+
+    rs <- dbDisconnect(pg)
+}
+
+
+process_calls <- function(num_calls = 1000, file_list = NULL) {
+    pg <- dbConnect(PostgreSQL())
+
+    call_files <- tbl(pg, sql("SELECT * FROM streetevents.call_files"))
+    calls <- tbl(pg, sql("SELECT * FROM streetevents.calls_new"))
+    speaker_data <- tbl(pg, sql("SELECT * FROM streetevents.speaker_data_new"))
+    speaker_data_dupes <- tbl(pg, sql("SELECT * FROM streetevents.speaker_data_dupes_new"))
 
     if (is.null(file_list)) {
 
         file_list <-
-            calls %>%
+            call_files %>%
+            inner_join(calls, by = "file_path") %>%
+            select(file_path, file_name, last_update) %>%
+            distinct() %>%
             arrange(random()) %>%
-            anti_join(speaker_data_alt, by = "file_path") %>%
-            anti_join(speaker_data_dupes, by = "file_path") %>%
+            anti_join(speaker_data, by = c("file_name", "last_update")) %>%
+            anti_join(speaker_data_dupes, by = c("file_name", "last_update")) %>%
             collect(n=num_calls)
     }
 
+    library(parallel)
     speaker_data <-
-        bind_rows(lapply(file_list$file_path, extract_speaker_data)) %>%
+        bind_rows(mclapply(file_list$file_path, extract_speaker_data, mc.cores=12)) %>%
         filter(speaker_text != "")
     print(sprintf("Speaker data has %d rows", nrow(speaker_data)))
 
@@ -153,8 +210,8 @@ process_calls <- function(num_calls = 1000, file_list = NULL) {
     } else {
         dupes <-
             speaker_data %>%
-            group_by(file_path, speaker_number, context, section) %>%
-            filter(n()>1) %>%
+            group_by(file_name, last_update, speaker_number, context, section) %>%
+            filter(n() > 1) %>%
             ungroup() %>%
             union_all(
                 speaker_data %>%
@@ -162,20 +219,23 @@ process_calls <- function(num_calls = 1000, file_list = NULL) {
 
         speaker_data <-
             speaker_data %>%
-            anti_join(dupes, by="file_path")
+            anti_join(dupes, by=c("file_name", "last_update"))
     }
 
     print("Writing data to Postgres")
+    rs <- dbGetQuery(pg, "SET TIME ZONE 'GMT'")
 
-    dbWriteTable(pg$con, c("streetevents", "speaker_data_alt"),
+    dbWriteTable(pg, c("streetevents", "speaker_data_new"),
                  speaker_data, row.names=FALSE, append=TRUE)
 
     print("Writing dupe data to Postgres")
-    file_path <- dupes %>% select(file_path) %>% distinct()
-    dbWriteTable(pg$con, c("streetevents", "speaker_data_dupes"), file_path,
+    file_path <- dupes %>% select(file_name, last_update) %>% distinct()
+    dbWriteTable(pg, c("streetevents", "speaker_data_dupes_new"), file_path,
                  row.names=FALSE, append=TRUE)
+    rs <- dbDisconnect(pg)
 }
 
-for (i in 1:10) {
-    system.time(process_calls(num_calls = 1000))
+for (i in 1:80) {
+    tm <- system.time(process_calls(num_calls = 4000))
+    print(tm)
 }
