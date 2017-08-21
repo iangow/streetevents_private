43dd71ec2af664c9e1b068fa81b93db84ba1fdc8
Ian Gow <iandgow@gmail.com>
2017-08-22 08:51:14 +1000

    Fixed bug introduced during merge.

--- import_speaker_data.R
+++ import_speaker_data.R
@@ -78,8 +78,9 @@ extract_speaker_data <- function(file_path) {
                     role <- NA
                 }
 
-                tibble(file_name, last_update, speaker_name, employer, role, speaker_number)
-            }
+                tibble(file_name, last_update, speaker_name,
+                       employer, role, speaker_number)
+        }
 
         pres <- sections[grepl("^(Presentation|Transcript)\n", sections)]
 
