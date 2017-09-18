4ba9cdc690471d4e852b5e58abcfafdd99833d92
Ian Gow <igow@hbs.edu>
2017-09-18 05:36:06 +0000

    Tweak to HBS download code to exclude *.bz2 files.

--- download_extract/update_streetevents.sh
+++ download_extract/update_streetevents.sh
@@ -2,7 +2,7 @@
 rsync -avz researchgrid.hbs.edu:/export/projects/streetevents_project/ \
     $EDGAR_DIR/streetevents_project/ --include=*.xml --exclude=*.zip \
     --exclude=*.7z --exclude=*.sas7bdat \
-    --exclude=*.xls* --exclude=jsheridan/* --delete
+    --exclude=*.xls* --exclude=*.bz2 --exclude=jsheridan/* --delete
 
 echo "Updating $PGDATABASE on $PGHOST from $EDGAR_DIR"
 Rscript --vanilla download_extract/create_call_files.R
