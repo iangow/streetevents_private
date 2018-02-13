80c2a881e790be3377f60eac888927db882e04df
Ian Gow <iandgow@gmail.com>
2018-02-13 15:26:42 +1100

    Tweaks to code creating selected_calls table.
    - Use 'search_path'
    - Disconnect from database; rm(pg) does not do this.
    - Removed some stray whitespace (done by RStudio automatically).
    - Closes #31.
    - Note that prior commit was cherry-picked from se_stage to master. Future commits should go to master branch.
    - @jingyujzhang.

--- create_selected_calls.R
+++ create_selected_calls.R
@@ -3,8 +3,9 @@ library(dplyr, warn.conflicts = FALSE)
 library(RPostgreSQL)
 pg <- dbConnect(PostgreSQL())
 
-crsp_link <- tbl(pg, sql("SELECT * FROM streetevents.crsp_link")) 
-calls <- tbl(pg, sql("select * from streetevents.calls")) 
+rs <- dbGetQuery(pg, "SET search_path='streetevents'")
+crsp_link <- tbl(pg, "crsp_link")
+calls <- tbl(pg, "calls")
 
 latest_calls <-
     calls %>%
@@ -16,23 +17,23 @@ latest_calls <-
     summarize(last_update = max(last_update, na.rm = TRUE),
               has_company_id = max(as.integer(!is.na(company_id)), na.rm = TRUE))
 
+dbGetQuery(pg, "DROP TABLE IF EXISTS selected_calls")
+
 selected_calls <-
     calls %>%
     mutate(has_company_id = as.integer(!is.na(company_id))) %>%
-    semi_join(latest_calls) %>% 
-    group_by(file_name) %>% 
+    semi_join(latest_calls) %>%
+    group_by(file_name) %>%
     summarize(file_path = max(file_path, na.rm = TRUE)) %>%
     inner_join(latest_calls) %>%
     select(file_name, file_path) %>%
     compute(name = "selected_calls", temporary = FALSE)
 
-dbGetQuery(pg, "DROP TABLE IF EXISTS streetevents.selected_calls")
-dbGetQuery(pg, "ALTER TABLE selected_calls SET SCHEMA streetevents")
-dbGetQuery(pg, "ALTER TABLE streetevents.selected_calls OWNER TO streetevents")
-dbGetQuery(pg, "GRANT SELECT ON TABLE streetevents.selected_calls TO streetevents_access")
+dbGetQuery(pg, "ALTER TABLE selected_calls OWNER TO streetevents")
+dbGetQuery(pg, "GRANT SELECT ON TABLE selected_calls TO streetevents_access")
 
 comment <- 'CREATED USING iangow/streetevents_private/create_selected_calls.R'
-sql <- paste0("COMMENT ON TABLE streetevents.selected_calls IS '",
+sql <- paste0("COMMENT ON TABLE selected_calls IS '",
               comment, " ON ", Sys.time() , "'")
 rs <- dbGetQuery(pg, sql)
-rm(pg)
+dbDisconnect(pg)
