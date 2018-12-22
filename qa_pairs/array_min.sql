8d35d2206673130e0cfc9f61af431207144a64c3
Ian Gow <iandgow@gmail.com>
2018-12-22 15:22:47 -0500

    Moved qa_pairs to non_answer repository.

--- qa_pairs/array_min.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE FUNCTION array_min(an_array integer[])
-  RETURNS integer AS
-$BODY$
-     WITH unnested AS (
-        SELECT UNNEST(an_array) AS ints)
-
-    SELECT min(ints)
-    FROM unnested
-$BODY$ LANGUAGE sql;
